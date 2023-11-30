a8b6b2ad16f72c748792a6316941d08ef47ddeec
jindongdu <djd28176@hotmail.com>
2023-11-30 16:39:05 -0500

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Jindong Du on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
