a581c4631ce65f62ae3b360fc4652eac59faa66b
jindongdu <djd28176@hotmail.com>
2023-12-01 10:17:50 -0500

    missing negavtive decimal percent functionality

---
+++ calculator/Model/calculatorModel.swift
@@ -0,0 +1,8 @@
+//
+//  calculatorModel.swift
+//  calculator
+//
+//  Created by Jindong Du on 11/30/23.
+//
+
+import Foundation
---
+++ calculator/Views/KeyView.swift
@@ -0,0 +1,18 @@
+//
+//  KeyView.swift
+//  calculator
+//
+//  Created by Jindong Du on 11/30/23.
+//
+
+import SwiftUI
+
+struct KeyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyView()
+}
